7c85bde4aca2a886a708a696fdefd204f06c71d0
cen1234 <mh3030027737@163.com>
2023-03-08 20:55:03 +0800

    建立登录与错误页

---
+++ Manage-fore/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// 子组件
+
+//引入组件
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+
+
+
+
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ErrorComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
