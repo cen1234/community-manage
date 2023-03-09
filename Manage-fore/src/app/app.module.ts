9e61e9f2712a42f6903b3ed06dfba80a2c7b5e60
cen1234 <mh3030027737@163.com>
2023-03-09 21:43:46 +0800

    完成错误页设置

--- Manage-fore/src/app/app.module.ts
+++ Manage-fore/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
@@ -11,11 +10,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // 子组件
-
-//引入组件
-import { NzButtonModule } from 'ng-zorro-antd/button';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
+//引入组件
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 
 
 
